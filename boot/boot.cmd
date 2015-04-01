5176e122157d8d2a3de96343b7918c5cc232aa9b
Mohammad Anwari <mdamt@mdamt.net>
2015-04-01 16:10:22 +0700

    Bring back rootwait

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 panic=10 init=/init ${extra}
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 rootwait panic=10 init=/init ${extra}
 ext2load mmc 0 0x43000000 boot/script.bin
 ext2load mmc 0 0x48000000 boot/uImage
 bootm 0x48000000
