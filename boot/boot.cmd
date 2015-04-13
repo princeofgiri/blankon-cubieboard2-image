38ef05916b1e8111af262725adb9c23258fe86e6
Mohammad Anwari <mdamt@mdamt.net>
2015-04-14 06:59:45 +0700

    Show console by default

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10  ${extra}
+setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10  ${extra}
 ext2load mmc 0 0x43000000 boot/script.bin
 ext2load mmc 0 0x48000000 boot/uImage
 ext2load mmc 0 0x49000000 boot/initramfs.uImage
